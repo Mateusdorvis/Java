4ee380ae88e76476d3acb77e51d1b89fc816aa78
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2025-07-18 18:12:04 +0000

    resds

--- Quiz.java
+++ Quiz.java
@@ -1,8 +1,15 @@
+import java.utils.*;
 public class Quiz[
+    public static void alternatives(String format, Object... arguments){
+        System.out.printf(format);
+    }
     public static void questions(){
-        
+        String q1,q2,q3,q4,q5;
+        /*PERGUNTAS */ 
+        q1 = "Qual é a capital da Inglaterra";
+
     }
     public static void main(String[] args){
-        /*PERGUNTAS */ 
+        
     }
 ]
